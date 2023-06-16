d23687b5cda56986fc308625d56ff3ee755ae41b
Vladislav-crypto <ferens.v@gmail.com>
2023-06-16 10:34:08 +0200

    working task 20,8 (need to fix bugs)

--- app/src/main/java/com/devferens/filmcollection/MainActivity.kt
+++ app/src/main/java/com/devferens/filmcollection/MainActivity.kt
@@ -2,10 +2,36 @@ package com.devferens.filmcollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.devferens.filmcollection.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun initButtons() {
+        binding.buttonMenu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonFav.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonWatchLater.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSelections.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSettings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
